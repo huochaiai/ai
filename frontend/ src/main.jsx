9d13897ce5324ff82e7d9aa24a35b7d4310bdd36
huochaiai <994388347@qq.com>
2026-03-23 03:08:57 +0800

    Refactor main.jsx to use ReactDOM for rendering

--- frontend/ src/main.jsx
+++ frontend/ src/main.jsx
@@ -1,3 +1,9 @@
-export default function App() {
-  return <h1>Hello, AI!</h1>;
-}
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
